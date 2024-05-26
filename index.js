2cff1842e3e05d7bc29210118b8db3d5064755ee
Josep23607 <168082443+Josep23607@users.noreply.github.com>
2024-05-26 19:17:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function canvia_seccio(num_boto) {
         let seccio = document.getElementById("seccio_" + i);
         if (i == num_boto) {
             boto.style.color = "#142890";    // es destaca la secció activa amb el canvi de colors del botó corresponent
-            boto.style.backgroundColor = "#FCDEE0";
+            boto.style.backgroundColor = "#0F1A2E";
             seccio.style.display = "flex";    // es fa visible la secció activa
         }
         else {
